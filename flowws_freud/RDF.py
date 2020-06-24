10a8f183c881f709ea11f92585fdb22ad5cfbdab
Matthew Spellings <mspells@umich.edu>
2020-06-24 09:52:29 -0400

    Added ability to draw bonds in RDF

--- flowws_freud/RDF.py
+++ flowws_freud/RDF.py
@@ -12,6 +12,10 @@ class RDF(flowws.Stage):
             help='Minimum radial distance'),
         Arg('r_max', type=float, required=True,
             help='Maximum radial distance'),
+        Arg('bond_max', type=float, default=0,
+            help='Render bonds that have length up to the given distance'),
+        Arg('bond_width', type=float, default=.1,
+            help='Width of drawn bonds, if enabled'),
     ]
 
     def run(self, scope, storage):
@@ -21,8 +25,28 @@ class RDF(flowws.Stage):
             self.arguments.get('r_min', 0))
 
         box = freud.box.Box.from_box(scope['box'])
-        compute.compute((box, scope['position']))
+        system = freud.locality.AABBQuery(box, scope['position'])
+        compute.compute(system)
         self.r, self.rdf = compute.bin_centers, compute.rdf
+
+        if self.arguments.get('bond_max', None):
+            import plato.draw
+
+            query_args = dict(
+                mode='ball', r_max=self.arguments['bond_max'], exclude_ii=True)
+            query = system.query(scope['position'], query_args)
+            nlist = query.toNeighborList()
+
+            start_points = scope['position'][nlist.query_point_indices]
+            bonds = (scope['position'][nlist.point_indices] - start_points)
+            bonds = box.wrap(bonds)
+            end_points = start_points + bonds
+
+            prim = plato.draw.Lines(
+                start_points=start_points, end_points=end_points,
+                widths=self.arguments['bond_width'], colors=(.1, .1, .1, 1),
+                cap_mode=1)
+            scope.setdefault('plato_primitives', []).append(prim)
 
         scope.setdefault('visuals', []).append(self)
 
